84061cac016b9376d76f42cb1afb5448d1ec712a
Deepak <115339084+rohitadarsh16@users.noreply.github.com>
2024-02-20 13:02:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ app.add_middleware(
 # port = 14636
 host = '193.203.166.103'
 database = 'u958929721_tools'
-user = 'u958929721_deftadm'
+user = 'u958929721_admin'
 password = 'Pr0h1b1t3d$'
 port = 3306
 

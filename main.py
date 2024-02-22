dcba703a721485ffbacf0539e09999b95fc08d48
Deepak <115339084+rohitadarsh16@users.noreply.github.com>
2024-02-22 15:12:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -86,4 +86,4 @@ async def get_feature_endpoint(platform_id: int = Query(...)):
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host='0.0.0.0', port=8000)
+    uvicorn.run(app, host='0.0.0.0', port=80)
